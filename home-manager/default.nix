a7643adbb0e5557b06512809c26ba9672b94c3e9
YoEight <yo.eight@gmail.com>
2024-05-16 09:12:58 -0400

    Enable signing all commits.

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -22,21 +22,21 @@ in
 
   programs.git = {
     enable = true;
-    # lfs.enable = true;
+    lfs.enable = true;
     userEmail = "yo.eight@gmail.com";
     userName = "YoEight";
-    # signing.signByDefault = true;
     extraConfig = {
       gpg.format = "ssh";
       user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtMMOPUb/XZ2wD/49TnTqWxcISgQDwgE4MdtvaCFOYF";
       gpg."ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+      commit.gpgsign = true;
     };
   };
 
   programs.ssh = {
     enable = true;
     forwardAgent = true;
-    extraConfig = "IdentityAgent ${_1passwordPath}";
+    extraConfig = "IdentityAgent ${config.home.homeDirectory}/.1password/agent.sock";
   };
 
   home.stateVersion = "23.11";
